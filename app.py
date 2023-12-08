57ee2db4844af8647b527526a1e804edbddcc54f
chris.cho <chris.cho@kakaocorp.com>
2023-12-08 19:28:18 +0900

    feat: Basic Chatbot Functionality via UI Module and OpenAIChatProcessor Integration

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
+import threading
+
 from ui.chat_interface import ChatInterface
 from config.open_api_config import initialize_openai_api
 
+from bot.openai_chat_processor import OpenAIChatProcessor
 # OpenAI API 초기화
 initialize_openai_api()
 
+# 비즈니스 로직을 별도의 함수로 정의
+def process_user_input(user_input, callback):
+    # 여기서 비동기적으로 처리 로직을 수행
+    message_log = [{'role': 'user', 'content': f'{user_input}'}]
+    response = chat_processor.process_chat(message_log)
+    callback(response)
+
+# ChatInterface에서 사용할 콜백 함수
 def on_send(user_input):
-    # 여기에 사용자 입력 처리 로직을 추가하세요.
-    print(f"Processing: {user_input}")
+    chat_interface.show_popup("처리 중입니다...")
+
+    def callback(response):
+        message = response.choices[0].message.content
+        print(f"user_input:{user_input} ==> {message}")
+
+        chat_interface.display_bot_message(message=message)
+        chat_interface.close_popup()
+
+    threading.Thread(target=process_user_input, args=(user_input, callback), daemon=True).start()
+
 
 def main():
+    global chat_interface, chat_processor
+
+    chat_processor = OpenAIChatProcessor("gpt-3.5-turbo")
     chat_interface = ChatInterface(send_callback=on_send)
     chat_interface.run()
 
--- ui/chat_interface.py
+++ ui/chat_interface.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from tkinter import Toplevel, Label
+
 
 class ChatInterface:
     def __init__(self, send_callback, title="GPT AI"):
@@ -8,6 +10,7 @@ class ChatInterface:
         self.window.title(title)
 
         self.setup_window()
+        self.display_bot_message("안녕하세요. 무엇을 도와드릴까요?")
 
     def setup_window(self):
         font = ("맑은 고딕", 10)
@@ -33,11 +36,25 @@ class ChatInterface:
         self.conversation.config(state=tk.DISABLED)
         self.conversation.see(tk.END)
 
+    def display_bot_message(self, message):
+        self.display_message(f"Bot: {message}\n", "assistant")
+
     def on_send(self):
         user_input = self.user_entry.get()
         self.user_entry.delete(0, tk.END)
         self.display_message(f"You: {user_input}\n", "user")
         self.send_callback(user_input)
 
+    def show_popup(self, message):
+        self.popup = Toplevel(self.window)
+        self.popup.title("Message")
+        Label(self.popup, text=message).pack(pady=10, padx=10)
+
+    def close_popup(self):
+        if self.popup:
+            self.popup.destroy()
+            self.popup = None
+
     def run(self):
         self.window.mainloop()
+
